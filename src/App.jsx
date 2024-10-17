83cb46548b1d20023273cc106b70b45344fedeae
StevenMadali <stevenmadali17@gmail.com>
2024-10-17 20:49:55 +0800

    Major revision due to adding search bar to search for pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,41 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Pokedex from './components/Pokedex'
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
+import axios from 'axios'
 
-const Trainers = [ {
-  name: 'Ash Ketchum',
-  age: 10,
-  hometown: 'Pallet Town',
-  starter: 'Pikachu'
-},
-{
-  name: 'Misty',
-  age: 12,
-  hometown: 'Cerulean City',
-  starter: 'Starmie'
-},
-{
-  name: 'Brock',
-  age: 15,
-  hometown: 'Pewter City',
-  starter: 'Onix'
-},
-{
-  name: 'Gary Oak',
-  age: 10,
-  hometown: 'Pallet Town',
-  starter: 'Eevee'
-}]
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // USE STATE DECLARATION
+  const [pokemonData, setpokemonData] = useState([null]);
+  const [error, seterror] = useState(null);
+
+
+  // SEARCH POKEMON FUNCTION
+  const searchPokemon = (name) => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)
+    .then((response) => {
+      setpokemonData(response.data);
+      seterror(null);
+    })
+    .catch((error) => {
+      seterror('Pokemon not found');
+      setpokemonData(null);
+    })
+  }
+
 
   return (
     <>
+    <div className="app">
+      <h1>Pokedex <CatchingPokemonIcon fontSize='large'/> </h1>
+      <sub>By steven madali</sub>
 
-    <h1>Trainers Information  <CatchingPokemonIcon fontSize="large"/></h1>
-
-    {/* USING ARRAY MAP */}
-    <h1>
-      {Trainers.map((trainer, index) => (
-        <div key={index}>
-          <h2>{trainer.name}</h2>
-          <p>Age: {trainer.age}</p>
-          <p>Hometown: {trainer.hometown}</p>
-          <p>Starter: {trainer.starter}</p>
-        </div>
-      ))}
-    </h1>
-
-    {/* <Pokedex/> */}
+      
+    </div>
+   
     </>
   )
 }
--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import '../App.css'
 import axios from 'axios'
+import '../assets/css/Pokedex.css'
+import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
+
 
 const Pokedex = () => {
+    // Use state 
     const [num, setnum] = useState();
     const [name, setname] = useState();
     const [move, setmove] = useState();
+    const [type, settype] = useState();
 
-
+    // Use effect 
     useEffect(() => {
         async function getData() {
             const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
@@ -20,11 +24,21 @@ const Pokedex = () => {
     })
 
   return (
-    <div>
-        <h1>POKEDEX</h1>
-        <h1>Pokemon ID No {num}</h1>
-        <h1>{name}</h1>
+    <div className='container'>
+        <h1>POKEDEX <CatchingPokemonIcon fontSize='large'/> </h1>
+
+        <input type="text" placeholder='Search Pokemon...' value={name} onChange={(event) => {
+          setname(event.target.value);
+        }}/>
+
         <h1>{name} number of moves is {move}</h1>
+
+
+        {/* <h1>Pokemon ID No {num}</h1>
+        <h1>{name}</h1>
+
+        <h1>{name} number of moves is {move}</h1>
+
       <select name="" id="" value={num} onChange={(event) => {
         setnum(event.target.value);
       }}>
@@ -35,7 +49,8 @@ const Pokedex = () => {
         <option value="4">4</option>   
         <option value="5">5</option>   
 
-      </select>
+      </select> */}
+
     </div>
   )
 }
